e27e1640830d5e5467fe16387345f68c118d609f
Idan Lev <idan.93lev@gmail.com>
2023-03-30 03:14:51 +0300

    updated social icons hover

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -4,7 +4,7 @@ const SocialMediaIcons = () => {
   return (
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
-        className="hover:opacity-50 transition duration-500"
+        className="hover:text-yellow transition duration-500"
         href="https://www.linkedin.com/in/idl808/"
         target="_blank"
         rel="noreferrer"
@@ -12,7 +12,7 @@ const SocialMediaIcons = () => {
         <BsLinkedin className="text-3xl"/>
       </a>
       <a
-        className="hover:opacity-50 transition duration-500"
+        className="hover:text-yellow transition duration-500"
         href="https://github.com/idan93l"
         target="_blank"
         rel="noreferrer"
@@ -20,7 +20,7 @@ const SocialMediaIcons = () => {
         <BsGithub className="text-3xl"/>
       </a>
       <a
-        className="hover:opacity-50 transition duration-500"
+        className="hover:text-yellow transition duration-500"
         href="https://www.facebook.com/idan.93lev"
         target="_blank"
         rel="noreferrer"
@@ -28,7 +28,7 @@ const SocialMediaIcons = () => {
         <BsFacebook className="text-3xl"/>
       </a>
       <a
-        className="hover:opacity-50 transition duration-500"
+        className="hover:text-yellow transition duration-500"
         href="https://www.instagram.com/idan_lev"
         target="_blank"
         rel="noreferrer"
@@ -36,7 +36,7 @@ const SocialMediaIcons = () => {
         <BsInstagram className="text-3xl"/>
       </a>
       <a
-        className="hover:opacity-50 transition duration-500"
+        className="hover:text-yellow transition duration-500"
         href="https://www.tiktok.com/@idl350"
         target="_blank"
         rel="noreferrer"
